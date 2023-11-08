d75f5e2b4bfee54277b439ad0d197a56535f3270
Nikola <nikola.janjic88rm@gmail.com>
2023-11-08 22:41:30 +0100

    highscore route fix

--- routes.php
+++ routes.php
@@ -3,7 +3,7 @@
 $router->get('/', 'Http/controllers/index.php');
 $router->get('/menu', 'Http/controllers/menu.php')->only('auth');
 $router->get('/game', 'Http/controllers/game.php')->only('auth');
-$router->get('/highscores', 'highscores.php')->only('auth');
+$router->get('/highscores', 'Http/controllers/highscores.php')->only('auth');
 
 $router->get('/login', 'Http/controllers/users/login/create.php')->only('guest');
 $router->post('/login', 'Http/controllers/users/login/store.php')->only('guest');
